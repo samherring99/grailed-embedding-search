43d3a27b707cceaa0a65f9cbf023c6c488e7845d
nightwing <samherring99@gmail.com>
2026-02-11 17:38:35 -0700

    Add proper exports to embeddings __init__.py
    - Export ProductEmbeddingModel, VectorStore, SimilaritySearch
    - Define __all__ for clean public API

---
+++ embeddings/__init__.py
@@ -0,0 +1,10 @@
+from .embedding_model import ProductEmbeddingModel
+from .vector_store import VectorStore
+from .similarity_search import SimilaritySearch
+
+__all__ = [
+    "ProductEmbeddingModel",
+    "VectorStore",
+    "SimilaritySearch",
+]
+
